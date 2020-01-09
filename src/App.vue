eadf74fccd1182aa2b6d7fd8b730858ddb32e72f
Diego Alvarez <dialvarezs@gmail.com>
2020-01-08 21:05:20 -0300

    Names component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>
+        Gato
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <names @players-setted="savePlayers"></names>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Names from "./components/Names.vue";
 
 export default {
-  name: "app",
+  name: "App",
   components: {
-    HelloWorld
+    Names
+  },
+  data() {
+    return {
+      players: {}
+    };
+  },
+  methods: {
+    savePlayers(data) {
+      this.players = data;
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Names.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-row justify="center" style="height: 300px">
+      <v-col cols="6" align-self="center">
+        <v-card outlined>
+          <v-card-title>
+            Jugadores
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="player1"
+              label="Jugador 1"
+              required
+            ></v-text-field>
+            <v-text-field
+              v-model="player2"
+              label="Jugador 2"
+              required
+            ></v-text-field>
+
+            <v-btn
+              class="mt-2"
+              outlined
+              block
+              color="primary"
+              @click="sendPlayerNames"
+            >
+              OK
+            </v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      player1: "",
+      player2: ""
+    };
+  },
+
+  methods: {
+    sendPlayerNames() {
+      this.$emit("players-setted", {
+        player1: this.player1,
+        player2: this.player2
+      });
+    }
+  }
+};
+</script>
+
+<style scoped></style>
